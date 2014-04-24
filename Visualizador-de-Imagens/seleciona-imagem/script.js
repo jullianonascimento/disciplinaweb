d276a0fb59fea32bc3192a39e663b26cbe45f85f
Julliano <julliano.rn@gmail.com>
2014-04-24 19:02:52 -0300

    Página do visualizador de imagens agora exibe imagem do computador e da URL digitada

--- Visualizador-de-Imagens/seleciona-imagem/script.js
+++ Visualizador-de-Imagens/seleciona-imagem/script.js
@@ -1,6 +1,26 @@
-function exibeImagem(){
-  alert("a imagem vem aqui");
-  var imagem = document.getElementById("arquivoDeImagem");
-  alert(imagem.height);
+/*função que obtem uma imagem de um arquivo no computador do usuário e a utiliza
+para alterar a imagem definida no elemento "minhaImagem" */
+function exibeImagemDoArquivo() {
+  var imgFile = document.getElementById("imagemDoArquivo");
+  if (imgFile.files && imgFile.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function(event) {
+        var dataUri = event.target.result,
+        img = document.createElement("img");
+        img.src = dataUri;
+        document.getElementById("minhaImagem").src = dataUri;
+     };
+     reader.onerror = function(event) {
+         console.error("File could not be read! Code " + event.target.error.code);
+     };
+     reader.readAsDataURL(imgFile.files[0]);
+  }
+}
+
+/*função que obtem uma URL e a utiliza para alterar 
+a imagem definida no elemento "minhaImagem" */
+function exibeImagemDaURL(){
+  var caminhoDaImagem = document.getElementById("textoDaURL").value;
+  document.getElementById("minhaImagem").src = caminhoDaImagem;
 }
 
